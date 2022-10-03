d5d853ac7c384a687b447d53940ea44fb645cd98
E-gap <106596181+E-gap@users.noreply.github.com>
2022-10-03 12:12:01 +0300

    02-task-complete

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const arrayLi = [];
+
+for (const ingredient of ingredients) {
+  
+  const el = document.createElement("li");
+  el.textContent = ingredient;
+  el.classList.add("item");
+  arrayLi.push(el);  
+
+}
+
+
+const ulIngredients = document.querySelector("#ingredients")
+
+ulIngredients.append(...arrayLi);
