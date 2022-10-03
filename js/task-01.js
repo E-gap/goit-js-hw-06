a19d6ffe291107b15ea055d1cdc82b6a5c5c7fda
E-gap <106596181+E-gap@users.noreply.github.com>
2022-10-03 10:45:54 +0300

    update-1

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const itemQuantity = document.querySelectorAll(".item");
+console.log(`Number of categories:`, itemQuantity.length);
+console.log(itemQuantity);
+
+const categories = document.querySelectorAll("h2");
+console.log(categories.value);
+
+
