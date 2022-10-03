063f5a649b9895cc33d2dc90a9876c0f1284c524
E-gap <106596181+E-gap@users.noreply.github.com>
2022-10-03 23:39:16 +0300

    update all

--- js/task-06.js
+++ js/task-06.js
@@ -7,10 +7,18 @@ const change = (event) => {
     const string = event.currentTarget.value;
     
     if (string.length === Number(lengthValue)) {
-        
+
+        console.log('fdgdf');
         length.classList.add("valid");
-    } else
-    length.classList.add("invalid");
-}
+        length.classList.remove("invalid");
+                
+
+    } else {
+        length.classList.add("invalid");
+        length.classList.remove("valid");
+    }
+    }
+    
+
 
 length.addEventListener(`blur`, change);
