f8ded7419b5fce34b3462eacd0c7f59456b02e11
E-gap <106596181+E-gap@users.noreply.github.com>
2022-10-03 21:46:23 +0300

    09-task-complete

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const color = document.querySelector('.color');
+
+button.addEventListener('click', () => {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+})
