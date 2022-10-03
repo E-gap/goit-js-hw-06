f44f6a540455e8378fcfda3ca9cdb22e534b9864
E-gap <106596181+E-gap@users.noreply.github.com>
2022-10-03 13:01:12 +0300

    03-task-complete

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,14 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+
+const imgLi = images.map(image =>  
+
+  `<li><img src="${image.url}" alt="${image.alt}"/></li>`).join("")
+
+
+const gallery = document.querySelector(".gallery");
+gallery.classList.add("display");
+
+gallery.insertAdjacentHTML("afterbegin", imgLi);
