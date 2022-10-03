50bd44eab5b551df4ac20b36c2b26762700bc834
E-gap <106596181+E-gap@users.noreply.github.com>
2022-10-03 20:52:36 +0300

    07-task-complete

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form')
+const email = document.querySelector('input[type="email"]')
+const password = document.querySelector('input[type="password"]')
 
+
+const object = {};
+
+const createObject = (event) => {
+    event.preventDefault(); 
+    if (email.value === "" || password.value === "") {
+        alert("Заповнені не всі обовязкові поля")
+    } else
+    
+    object.email = email.value;
+    object.password = password.value;
+    
+    console.log(object);
+    event.currentTarget.reset();
+}
+
+form.addEventListener('submit', createObject);
