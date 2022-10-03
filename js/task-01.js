303864e5e0faa3538ee92c21ac436fe3dfe550bd
E-gap <106596181+E-gap@users.noreply.github.com>
2022-10-03 11:11:19 +0300

    01-task-complete

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,13 @@
-const itemQuantity = document.querySelectorAll(".item");
-console.log(`Number of categories:`, itemQuantity.length);
-console.log(itemQuantity);
-
-const categories = document.querySelectorAll("h2");
-console.log(categories.value);
+const items = document.querySelectorAll(".item");
+console.log(`Number of categories:`, items.length);
 
 
+
+for (const item of items) {
+   
+    const category = item.querySelector("h2");
+    console.log(`Category:`, category.textContent);    
+    const elements = item.querySelectorAll("li");
+    console.log(`Elements:`, elements.length);
+
+};
